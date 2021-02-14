e1a7ff0866cfdad8ce2177026bdfe635cd8ee7eb
Kerry Perret <kerry.perret@outlook.com>
2021-02-14 14:41:29 +0100

    meow

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,7 +1,7 @@
 $(function(){ 
     // Right o' wrong...
     // $('body').niceScroll();
-
+    console.info("-- I'm not a Front-End Developer! --");
     window.setupBackToTopButton();
     window.setupPageProgressBar('#header', '#post');
 
